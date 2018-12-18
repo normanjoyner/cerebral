1e4f0c035ead74629cc0c700b7c18d5ade56d8d6
Norman Joyner <norman.joyner@gmail.com>
2018-12-17 22:19:49 -0500

    update NewClient signature and rename variables

--- pkg/autoscaling/engines/containership/containership.go
+++ pkg/autoscaling/engines/containership/containership.go
@@ -31,28 +31,30 @@ type cloudConfig struct {
 }
 
 // NewClient creates a new instance of the containership AutoScaling Engine, or an error
-func NewClient(name string, address string, tokenEnvVarName string, clusterID string, organizationID string) (autoscaling.Engine, error) {
+// It is expected that we should not modify the name or configuration here as the caller
+// may not have passed a DeepCopy
+func NewClient(name string, configuration map[string]string) (autoscaling.Engine, error) {
 	if name == "" {
 		return nil, errors.New("name must be provided")
 	}
 
-	if address == "" {
+	if configuration["address"] == "" {
 		return nil, errors.New("address must be provided")
 	}
 
-	if tokenEnvVarName == "" {
+	if configuration["tokenEnvVarName"] == "" {
 		return nil, errors.New("tokenEnvVarName must be provided")
 	}
 
-	if clusterID == "" {
-		return nil, errors.New("clusterID must be provided")
-	}
-
-	if organizationID == "" {
+	if configuration["organizationID"] == "" {
 		return nil, errors.New("organizationID must be provided")
 	}
 
-	token := os.Getenv(tokenEnvVarName)
+	if configuration["clusterID"] == "" {
+		return nil, errors.New("clusterID must be provided")
+	}
+
+	token := os.Getenv(configuration["tokenEnvVarName"])
 	if token == "" {
 		return nil, errors.New("unable to get Containership Cloud API cluster token")
 	}
@@ -60,7 +62,7 @@ func NewClient(name string, address string, tokenEnvVarName string, clusterID st
 	// TODO: is there anyway to test this without a real token?
 	cloudclientset, err := cscloud.New(cscloud.Config{
 		Token:            token,
-		ProvisionBaseURL: address,
+		ProvisionBaseURL: configuration["address"],
 	})
 	if err != nil {
 		return nil, errors.New("unable to create containership cloud clientset")
@@ -69,22 +71,22 @@ func NewClient(name string, address string, tokenEnvVarName string, clusterID st
 	return Engine{
 		name: name,
 		config: &cloudConfig{
-			Address:         address,
-			TokenEnvVarName: tokenEnvVarName,
-			ClusterID:       clusterID,
-			OrganizationID:  organizationID,
+			Address:         configuration["address"],
+			TokenEnvVarName: configuration["tokenEnvVarName"],
+			ClusterID:       configuration["clusterID"],
+			OrganizationID:  configuration["organizationID"],
 		},
 		cloud: cloudclientset,
 	}, nil
 }
 
 // Name returns the name of the engine
-func (cae Engine) Name() string {
-	return cae.name
+func (e Engine) Name() string {
+	return e.name
 }
 
 // SetTargetNodeCount takes action to scale a target node pool
-func (cae Engine) SetTargetNodeCount(nodeSelectors map[string]string, numNodes int, strategy string) (bool, error) {
+func (e Engine) SetTargetNodeCount(nodeSelectors map[string]string, numNodes int, strategy string) (bool, error) {
 	if numNodes < 0 {
 		return false, errors.New("cannot scale below 0")
 	}
@@ -94,12 +96,12 @@ func (cae Engine) SetTargetNodeCount(nodeSelectors map[string]string, numNodes i
 		return false, errors.New("could not get autoscaling group node pool ID")
 	}
 
-	log.Infof("Containership AutoscalingEngine %s is requesting Containership Cloud to set target nodes %v to %d", cae.Name(), nodeSelectors, numNodes)
+	log.Infof("Containership AutoscalingEngine %s is requesting Containership Cloud to set target nodes %v to %d", e.Name(), nodeSelectors, numNodes)
 
 	switch strategy {
 	case "random", "":
 		// random is the default for this engine
-		return cae.scaleStrategyRandom(id, numNodes)
+		return e.scaleStrategyRandom(id, numNodes)
 	default:
 		return false, errors.Errorf("unable to scale node pool using strategy %s", strategy)
 	}
@@ -108,13 +110,13 @@ func (cae Engine) SetTargetNodeCount(nodeSelectors map[string]string, numNodes i
 // ScaleStrategyRandom take in the number of desired nodes for a node pool.
 // It then makes a request to Containership Cloud API to set the node pool to
 // the desired count
-func (cae Engine) scaleStrategyRandom(nodePoolID string, numNodes int) (bool, error) {
+func (e Engine) scaleStrategyRandom(nodePoolID string, numNodes int) (bool, error) {
 	target := int32(numNodes)
 	req := types.ScaleNodePoolRequest{
 		Count: &target,
 	}
 
-	_, err := cae.cloud.Provision().NodePools(cae.config.OrganizationID, cae.config.ClusterID).Scale(nodePoolID, &req)
+	_, err := e.cloud.Provision().NodePools(e.config.OrganizationID, e.config.ClusterID).Scale(nodePoolID, &req)
 	if err != nil {
 		return false, errors.Wrap(err, "There was an error scaling autoscaling group")
 	}
