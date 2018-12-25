85fdb91f4c4fbb7e81380fd28e911ffe95bcc22f
Norman Joyner <norman.joyner@gmail.com>
2018-12-25 11:48:41 -0500

    add InfluxDB metrics backend tests

---
+++ pkg/metrics/backends/influxdb/metrics_test.go
@@ -0,0 +1,44 @@
+package influxdb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultAndValidate(t *testing.T) {
+	c := metricConfiguration{}
+	err := c.defaultAndValidate(nil)
+	assert.NoError(t, err, "nil config provided is ok")
+	assert.Equal(t, defaultAggregation, c.Aggregation, "aggregation defaulted")
+	assert.Equal(t, defaultRange, c.Range, "range defaulted")
+
+	c = metricConfiguration{}
+	err = c.defaultAndValidate(map[string]string{})
+	assert.NoError(t, err, "empty config provided is ok")
+	assert.Equal(t, defaultAggregation, c.Aggregation, "aggregation defaulted")
+	assert.Equal(t, defaultRange, c.Range, "range defaulted")
+
+	c = metricConfiguration{}
+	err = c.defaultAndValidate(map[string]string{
+		"aggregation": "max",
+		"range":       "5m",
+	})
+	assert.NoError(t, err, "good config")
+	assert.Equal(t, "max", c.Aggregation, "aggregation not defaulted if provided")
+	assert.Equal(t, "5m", c.Range, "range not defaulted if provided")
+
+	c = metricConfiguration{}
+	err = c.defaultAndValidate(map[string]string{
+		"aggregation": "BADBADNOTGOOD",
+		"range":       "5m",
+	})
+	assert.Error(t, err, "bad aggregation")
+
+	c = metricConfiguration{}
+	err = c.defaultAndValidate(map[string]string{
+		"aggregation": "max",
+		"range":       "BADBADNOTGOOD",
+	})
+	assert.Error(t, err, "bad range")
+}
