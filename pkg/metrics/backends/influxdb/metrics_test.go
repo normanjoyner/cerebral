c27fc65e02ff986dcc34603d81b937605999be00
Norman Joyner <norman.joyner@gmail.com>
2018-12-25 12:28:59 -0500

    fix metrics tests

--- pkg/metrics/backends/influxdb/metrics_test.go
+++ pkg/metrics/backends/influxdb/metrics_test.go
@@ -12,33 +12,46 @@ func TestDefaultAndValidate(t *testing.T) {
 	assert.NoError(t, err, "nil config provided is ok")
 	assert.Equal(t, defaultAggregation, c.Aggregation, "aggregation defaulted")
 	assert.Equal(t, defaultRange, c.Range, "range defaulted")
+	assert.Equal(t, defaultDB, c.DB, "db defaulted")
+	assert.Equal(t, defaultRetentionPolicy, c.RetentionPolicy, "retention policy defaulted")
 
 	c = metricConfiguration{}
 	err = c.defaultAndValidate(map[string]string{})
 	assert.NoError(t, err, "empty config provided is ok")
 	assert.Equal(t, defaultAggregation, c.Aggregation, "aggregation defaulted")
 	assert.Equal(t, defaultRange, c.Range, "range defaulted")
+	assert.Equal(t, defaultDB, c.DB, "db defaulted")
+	assert.Equal(t, defaultRetentionPolicy, c.RetentionPolicy, "retention policy defaulted")
 
 	c = metricConfiguration{}
 	err = c.defaultAndValidate(map[string]string{
-		"aggregation": "max",
-		"range":       "5m",
+		"aggregation":     "max",
+		"range":           "5m",
+		"db":              "eh",
+		"retentionPolicy": "p_100d",
 	})
 	assert.NoError(t, err, "good config")
 	assert.Equal(t, "max", c.Aggregation, "aggregation not defaulted if provided")
 	assert.Equal(t, "5m", c.Range, "range not defaulted if provided")
+	assert.Equal(t, "eh", c.DB, "db not defaulted if provided")
+	assert.Equal(t, "p_100d", c.RetentionPolicy, "retention policy not defaulted if provided")
 
 	c = metricConfiguration{}
 	err = c.defaultAndValidate(map[string]string{
-		"aggregation": "BADBADNOTGOOD",
-		"range":       "5m",
+		"aggregation":     "BADBADNOTGOOD",
+		"range":           "5m",
+		"db":              "eh",
+		"retentionPolicy": "p_100d",
 	})
 	assert.Error(t, err, "bad aggregation")
 
 	c = metricConfiguration{}
 	err = c.defaultAndValidate(map[string]string{
-		"aggregation": "max",
-		"range":       "BADBADNOTGOOD",
+		"aggregation":     "max",
+		"range":           "BADBADNOTGOOD",
+		"db":              "eh",
+		"retentionPolicy": "p_100d",
 	})
 	assert.Error(t, err, "bad range")
+
 }
